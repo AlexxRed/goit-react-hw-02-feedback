589f4dac4d5a181f07ff8df391ab5ec9cbed9c8b
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-02-23 22:51:07 +0200

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -4,18 +4,52 @@ import { Component } from 'react';
 
 
 class FeedbackOptions extends Component {
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         value: 0,
+    //     }
+    // };
+    state = {
+        goodValue: 0,
+        neutralValue:0,
+        badValue: 0,
+    };
 
 
     handleIncrement = (e) => {
-        console.log("click")
-        console.log(e);
+        const buttonName = e.target.name
+        console.log(buttonName);
+        if (buttonName === 'Good') {
+            this.setState({
+                goodValue: 2,
+            })
+        }if (buttonName === 'Neutral') {
+            this.setState({
+                neutralValue: 2,
+            })
+        }if (buttonName === 'Bad') {
+            this.setState({
+                badValue: 2,
+            })
+        }
+
+        console.log(this.state);
     }
 
     render() {
         return <div className=''>
-            <button type='button' onClick={this.handleIncrement}>Good</button>
-        <button type='button'  onClick={this.handleIncrement}>Neutral</button>
-        <button type='button'  onClick={this.handleIncrement}>Bad</button>
+        <button type='button' name='Good' onClick={this.handleIncrement}>Good</button>
+        <button type='button' name='Neutral' onClick={this.handleIncrement}>Neutral</button>
+        <button type='button' name='Bad' onClick={this.handleIncrement}>Bad</button>
+            <div style={{
+                display: 'flex',
+                marginLeft: '30px'
+            }}>
+            <div>{this.state.goodValue}</div>
+            <div>{this.state.neutralValue}</div>
+            <div>{this.state.badValue}</div>
+        </div>
     </div>
     }
 }
